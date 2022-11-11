b2a0e19193ff683b005b90a566bb5a6510aeb861
tsioko01 <82219444+tsioko01@users.noreply.github.com>
2022-11-11 17:38:38 +0200

    Update search.go

--- internal/search/search.go
+++ internal/search/search.go
@@ -104,7 +104,7 @@ func (s *Search) initRateClient() error {
 // Nearby returns ids of nearby hotels ordered by ranking algo
 func (s *Search) Nearby(ctx context.Context, req *pb.NearbyRequest) (*pb.SearchResult, error) {
 	// find nearby hotels
-	nearby, err := s.geo.Nearby(&geo.Request{
+	nearby, err := s.geoClient.Nearby(&geo.Request{
 		Lat: req.Lat,
 		Lon: req.Lon,
 	})
@@ -113,7 +113,7 @@ func (s *Search) Nearby(ctx context.Context, req *pb.NearbyRequest) (*pb.SearchR
 	}
 
 	// find rates for hotels
-	rates, err := s.rate.GetRates(&rate.RateRequest{
+	rates, err := s.rateClient.GetRates(&rate.RateRequest{
 		HotelIds: nearby.HotelIds,
 		InDate:   req.InDate,
 		OutDate:  req.OutDate,
